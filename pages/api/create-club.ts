cfc3ab0d10052298de732c8b4ef919a01084c7a9
elwinjyot <elwinjyothisr444@gmail.com>
2023-04-13 15:45:49 +0530

    6

--- pages/api/create-club.ts
+++ pages/api/create-club.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
+  // if (req.method === "POST") {
     const { clubName, img } = JSON.parse(req.body);
 
     const response = await prisma.club.create({
@@ -24,7 +24,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(200).send(clubs);
       }
     }
-  } else {
-    res.status(400).send({ message: "Only post request accepted" });
-  }
+  // } else {
+    // res.status(400).send({ message: "Only post request accepted" });
+  // }
 } 
