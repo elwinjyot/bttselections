9c22426b0ac3bca240575a88f547d840201f01ab
Elwin Jyothis <elwinjyothisr444@gmail.com>
2023-04-17 09:53:06 +0530

    Update map.ts

--- lib/map.ts
+++ lib/map.ts
@@ -5,10 +5,17 @@
 
 const pageMap: { [key: number]: string[] } = {
     1: ["clgjl7lgn0004la084unzoc40", "clgjl8qpl0006la089t6pf9rr", "clgjmjc9l0004jo0828vb0f8y"],
-    2: ["clgjld58m000ala08i24ny0b2", "clgjldnj8000cla08j40yub1b"],
-    3: ["clgjlfgdz000ela08a33zhpuh"],
-    4: ["clgjlr1qf0000l108i5v3gx3q", "clgjlrhyu0002l108h70ih1su"],
-    5: ["clgjmhez20002jo08ksby86cn", "clgjl5qjv0002la087xfzl2v3", "clgjlbrhf0008la089hruql64"],
+    2: ["clgjld58m000ala08i24ny0b2", "clgjldnj8000cla08j40yub1b", "clgjqcgz50014mf086szh3qsx"],
+    3: ["clgjlfgdz000ela08a33zhpuh", "clgjr8yc5000mjw08uh2n4veb", "clgjr9qrs000ojw08p3tvic5r"],
+    4: ["clgjlr1qf0000l108i5v3gx3q", "clgjlrhyu0002l108h70ih1su", "clgjreawo000ujw08uk83tn03"],
+    5: ["clgjn80ed0000lf08a12l3fcl", "clgjn93f40002lf083ti1itrm", "clgjna2ek0004lf08c2rwxuvn"],
+    6: ["clgjnbbip0006lf087cp5jngv", "clgjnbq0k0008lf08k6c10i424", "clgjncau4000alf08upsnidmf"],
+    7: ["clgjnd43j000clf08235xia3q", "clgjndle4000elf08mwtqb4fp", "clgjne147000glf08atwsjwbj"],
+    8: ["clgjng4kp000ilf08jsrjeu7b", "clgjngs7v000klf08bnaq3xpt", "clgjnhe0i000mlf08lznxt4hq"],
+    9: ["clgjnik2z000olf08r4sdurfc", "clgjnj9og000qlf085mirtgcg", "clgjnke9c000slf08hkvbw0pa"],
+    10: ["clgjnlfwa000ulf08xcyiznsw", "clgjnm0cz000wlf08no8iwmjh", "clgjnmjuj000ylf08yw91sqju"],
+    11: ["clgjnnh940010lf08nbii2im8", "clgjno5a80012lf08aqdbw5zh", "clgjnotpb0014lf083wn12bvg"],
+    12: ["clgjmhez20002jo08ksby86cn", "clgjl5qjv0002la087xfzl2v3", "clgjlbrhf0008la089hruql64"]
 }
 
 export default pageMap;
